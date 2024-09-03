a3943c0f593f5112d193630c93e312807c0ab230
Tommy <trickytommy4@gmail.com>
2024-09-03 11:52:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React</h1>
+        <nav>
+          <ul>
+            <li> <a href='#'>Home</a> </li>
+            <li> <a href='#'>Products</a> </li>
+            <li> <a href='#'>About</a> </li>
+            <li> <a href='#'>Contact</a> </li>
+          </ul>
+        </nav>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
